618f81fd2dfe69d2184633f6154a4ee01808a236
VopRos-ZP <zolpavel366@gmail.com>
2022-11-27 16:45:59 +0300

    Delete unused

--- app/src/main/java/com/bulkapedia/sets/PlayMode.kt
+++
@@ -1,9 +0,0 @@
-package com.bulkapedia.sets
-
-import com.bulkapedia.R
-
-enum class PlayMode (val str: Int) {
-    SQUAD(R.string.squad_mode),
-    BATTLE_ROYAL(R.string.battle_royale_mode),
-    ALL(R.string.all_modes);
-}
