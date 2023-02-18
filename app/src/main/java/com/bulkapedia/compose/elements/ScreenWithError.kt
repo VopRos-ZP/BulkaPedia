5b8d3cda8526c1eb1da54d2a99aebb3331793e35
VopRos-ZP <zolpavel366@gmail.com>
2023-02-18 17:59:49 +0300

    changed alignment

--- app/src/main/java/com/bulkapedia/compose/elements/ScreenWithError.kt
+++ app/src/main/java/com/bulkapedia/compose/elements/ScreenWithError.kt
@@ -1,12 +1,13 @@
 @file:Suppress("FunctionName")
 package com.bulkapedia.compose.elements
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.bulkapedia.compose.util.CenteredBox
 
 @Composable
 fun ScreenWithError(content: @Composable (ScreenAction.ErrorAction) -> Unit) {
@@ -15,8 +16,9 @@ fun ScreenWithError(content: @Composable (ScreenAction.ErrorAction) -> Unit) {
     val onClose = remember { mutableStateOf({}) }
 
     val action = ScreenAction.ErrorAction(show, text, onClose)
-    CenteredBox (
-        modifier = Modifier.fillMaxSize()
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
     ) {
         content.invoke(action)
         if (show.value) {
