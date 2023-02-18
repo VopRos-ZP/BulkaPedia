04f6a08cc7d7c39264695207ec6d80796705eb5c
VopRos-ZP <zolpavel366@gmail.com>
2023-02-18 16:09:03 +0300

    removed horizontal padding

--- app/src/main/java/com/bulkapedia/compose/MainActivity.kt
+++ app/src/main/java/com/bulkapedia/compose/MainActivity.kt
@@ -11,13 +11,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -100,8 +98,7 @@ fun MainScreen(
             backgroundColor = PrimaryDark,
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.Transparent)
-                .padding(horizontal = 20.dp)
+                .background(PrimaryDark)
                 .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
         ) {
             val navBackStackEntry by childNC.currentBackStackEntryAsState()
--- app/src/main/java/com/bulkapedia/compose/elements/Toolbar.kt
+++ app/src/main/java/com/bulkapedia/compose/elements/Toolbar.kt
@@ -1,24 +1,18 @@
+@file:Suppress("FunctionName")
 package com.bulkapedia.compose.elements
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.unit.dp
 import com.bulkapedia.R
 import com.bulkapedia.compose.navigation.Destinations
@@ -31,12 +25,11 @@ import com.bulkapedia.compose.util.clickable
 @Composable
 fun Toolbar(ctx: ToolbarCtx) {
     val viewState: State<ToolbarData?> = ctx.observeAsState()
-
     Row (
         modifier = Modifier.fillMaxWidth()
             .height(50.dp)
-            .padding(horizontal = 20.dp)
-            .background(PrimaryDark, RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+            .background(PrimaryDark)
+            .clip(RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
             .padding(vertical = 10.dp)
     ) {
         if (viewState.value?.showBackButton == true) {
