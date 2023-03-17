de983d9b9bb4eec8f5610da473bdfcec28a9ccc2
VopRos-ZP <zolpavel366@gmail.com>
2023-03-17 19:09:43 +0300

    updated SearchView

--- app/src/main/java/com/bulkapedia/compose/elements/SearchView.kt
+++ app/src/main/java/com/bulkapedia/compose/elements/SearchView.kt
@@ -2,23 +2,22 @@
 package com.bulkapedia.compose.elements
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bulkapedia.compose.ui.theme.PrimaryDark
 import com.bulkapedia.compose.ui.theme.Teal200
 
@@ -30,8 +29,9 @@ fun SearchView(state: MutableState<String>) {
             state.value = value
         },
         modifier = Modifier
-            .fillMaxWidth(),
-        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(horizontal = 20.dp),
         leadingIcon = {
             Icon(
                 Icons.Filled.Search,
@@ -45,7 +45,7 @@ fun SearchView(state: MutableState<String>) {
             if (state.value != "") {
                 IconButton(onClick = { state.value = "" }) {
                     Icon(
-                        Icons.Filled.Close,
+                        Icons.Outlined.Close,
                         contentDescription = "",
                         modifier = Modifier
                             .padding(15.dp)
@@ -55,7 +55,7 @@ fun SearchView(state: MutableState<String>) {
             }
         },
         singleLine = true,
-        shape = RectangleShape,
+        shape = RoundedCornerShape(50.dp),
         colors = TextFieldDefaults.textFieldColors(
             textColor = Teal200,
             cursorColor = Teal200,
