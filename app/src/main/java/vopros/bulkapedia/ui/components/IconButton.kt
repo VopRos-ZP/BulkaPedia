aa4a62b9d5dcc8270d68f135904f5cc8876df868
Павел <zolpavel366@gmail.com>
2023-10-03 21:55:50 +0300

    added `IconButton`

---
+++ app/src/main/java/vopros/bulkapedia/ui/components/IconButton.kt
@@ -0,0 +1,27 @@
+package vopros.bulkapedia.ui.components
+
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import vopros.bulkapedia.ui.theme.BulkaPediaTheme
+
+@Composable
+fun IconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    icon: ImageVector
+) {
+    androidx.compose.material.IconButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = BulkaPediaTheme.colors.tintColor
+        )
+    }
+}
