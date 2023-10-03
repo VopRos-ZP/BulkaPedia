4611acad7bd7e1f3de9806d1c6c8bc4744674411
Павел <zolpavel366@gmail.com>
2023-10-03 21:57:35 +0300

    updated `TopBar`

--- app/src/main/java/vopros/bulkapedia/ui/components/topbar/TopBar.kt
+++ app/src/main/java/vopros/bulkapedia/ui/components/topbar/TopBar.kt
@@ -1,8 +1,5 @@
 package vopros.bulkapedia.ui.components.topbar
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,7 +7,15 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import vopros.bulkapedia.R
+import vopros.bulkapedia.ui.components.IconButton
+import vopros.bulkapedia.ui.components.Text
 import vopros.bulkapedia.ui.navigation.Destinations
+import vopros.bulkapedia.ui.theme.BulkaPediaTheme
+import vopros.bulkapedia.ui.theme.BulkapediaTheme
 import vopros.bulkapedia.ui.theme.LocalTopBarViewModel
 
 @Composable
@@ -20,28 +25,38 @@ fun TopBar() {
     val title by viewModel.title.collectAsState()
     val showBack by viewModel.showBack.collectAsState()
     val controller by viewModel.navController.collectAsState()
+    TopBar(title = title, showBack = showBack, controller = controller)
+}
 
+@Preview(showBackground = true)
+@Composable
+fun TopBar_Preview() {
+    BulkapediaTheme {
+        TopBar(
+            title = stringResource(R.string.alice),
+            showBack = true,
+            controller = null
+        )
+    }
+}
+
+@Composable
+fun TopBar(title: String, showBack: Boolean, controller: NavController?) {
     TopAppBar(
-        title = { Text(text = title) },
+        backgroundColor = BulkaPediaTheme.colors.primary,
+        title = { Text(title = title) },
         navigationIcon = {
             if (showBack) {
-                IconButton(onClick = { controller?.navigateUp() }) {
-                    Icon(
-                        Icons.Filled.ArrowBack,
-                        contentDescription = null
-                    )
-                }
+                IconButton(
+                    onClick = { controller?.navigateUp() },
+                    icon = Icons.Filled.ArrowBack
+                )
             }
         },
         actions = {
             IconButton(onClick = {
                 controller?.navigate(Destinations.SETTINGS) { launchSingleTop = true }
-            }) {
-                Icon(
-                    Icons.Filled.Settings,
-                    contentDescription = null
-                )
-            }
+            }, icon = Icons.Filled.Settings)
         }
     )
 }
