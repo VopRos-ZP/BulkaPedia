ca30bbaacdf9fc2393ae033ac286de1fcb678774
Павел <zolpavel366@gmail.com>
2023-10-04 10:34:10 +0300

    updated `Card` and `OutlinedCard`

--- app/src/main/java/vopros/bulkapedia/ui/components/cards/Card.kt
+++ app/src/main/java/vopros/bulkapedia/ui/components/cards/Card.kt
@@ -6,27 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import vopros.bulkapedia.R
-import vopros.bulkapedia.ui.components.Image
-import vopros.bulkapedia.ui.components.Text
-import vopros.bulkapedia.ui.theme.BulkapediaTheme
-
-@Preview(showBackground = true)
-@Composable
-fun WidthCard_Preview() {
-    Card {
-        Text(resource = R.string.arnie)
-        Image(url = "https://firebasestorage.googleapis.com/v0/b/bulkapedia-3b45a.appspot.com/o/heroes%2Fscouts%2Falice_icon.jpg?alt=media&token=2ee3ebef-ac18-4bb3-b550-4d64f701a479")
-    }
-}
+import vopros.bulkapedia.ui.theme.BulkaPediaTheme
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -38,10 +26,10 @@ fun OutlinedCard(
 ) {
     androidx.compose.material.Card(
         onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp),
-        border = BorderStroke(2.dp, MaterialTheme.colors.primary)
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier.fillMaxWidth(),
+        backgroundColor = Color.Transparent,
+        border = BorderStroke(2.dp, BulkaPediaTheme.colors.primary)
     ) {
         Column(
             modifier = Modifier.padding(10.dp),
@@ -62,9 +50,9 @@ fun Card(
 ) {
     androidx.compose.material.Card(
         onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp),
+        backgroundColor = BulkaPediaTheme.colors.primary,
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier.fillMaxWidth(),
     ) {
         Column(
             modifier = Modifier.padding(10.dp),
