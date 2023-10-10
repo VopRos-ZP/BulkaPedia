076680ad11e5beb45b489044aa7a5770a692d1a0
VopRos-ZP <zolpavel366@gmail.com>
2023-10-10 17:03:46 +0300

    added `CreateSet` Destination

--- app/src/main/java/vopros/bulkapedia/ui/screens/createSet/CreateSetScreen.kt
+++ app/src/main/java/vopros/bulkapedia/ui/screens/createSet/CreateSetScreen.kt
@@ -1,28 +1,52 @@
 package vopros.bulkapedia.ui.screens.createSet
 
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
 import vopros.bulkapedia.R
+import vopros.bulkapedia.ui.components.Loading
+import vopros.bulkapedia.ui.components.ScreenView
 import vopros.bulkapedia.ui.components.Text
-import vopros.bulkapedia.ui.components.userSet.Gears
 import vopros.bulkapedia.ui.screens.hero.HeroThumbnail
-import vopros.bulkapedia.userSet.UserSetUseCase
 
+@Destination
 @Composable
-fun CreateSetScreen(heroId: String, setId: String?) {
-//    Screen<UserSetUseCase, CreateSetViewModel>(
-//        title = R.string.create_set,
-//        showBack = true,
-//        fetch = { startIntent(CreateSetViewIntent.Start(heroId, setId)) }
-//    ) { _, useCase ->
-//        HeroThumbnail(image = useCase.hero.image) {
-//            OutlinedButton(onClick = { /*TODO*/ }) {
-//                Text(R.string.save)
-//            }
-//        }
-//        Card {
-//            Gears(useCase.set.gears)
-//        }
-//    }
+fun CreateSetScreen(
+    viewModel: CreateSetViewModel,
+    heroId: String,
+    setId: String?
+) {
+    val useCase by viewModel.useCase.collectAsState()
+    ScreenView(
+        title = R.string.create_set,
+        showBack = true,
+        viewModel = viewModel
+    ) {
+        when (val case = useCase) {
+            null -> Loading()
+            else -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    item {
+                        HeroThumbnail(image = case.hero.image) {
+                            OutlinedButton(onClick = { viewModel.saveSet(case.set) }) {
+                                Text(R.string.save)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    LaunchedEffect(heroId, setId) { viewModel.init(heroId, setId) }
 }
--- app/src/main/java/vopros/bulkapedia/ui/screens/createSet/CreateSetViewModel.kt
+++ app/src/main/java/vopros/bulkapedia/ui/screens/createSet/CreateSetViewModel.kt
@@ -1,13 +1,15 @@
 package vopros.bulkapedia.ui.screens.createSet
 
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import vopros.bulkapedia.hero.HeroRepository
 import vopros.bulkapedia.storage.DataStore
+import vopros.bulkapedia.ui.view.ErrViewModel
 import vopros.bulkapedia.user.UserRepository
 import vopros.bulkapedia.userSet.SetRepository
 import vopros.bulkapedia.userSet.UserSet
 import vopros.bulkapedia.userSet.UserSetUseCase
-import dagger.hilt.android.lifecycle.HiltViewModel
-import vopros.bulkapedia.ui.view.ErrViewModel
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,24 +18,28 @@ class CreateSetViewModel @Inject constructor(
     private val setRepository: SetRepository,
     private val userRepository: UserRepository,
     private val heroRepository: HeroRepository,
-): ErrViewModel() {
+) : ErrViewModel() {
 
-//    override var reducer: Reducer<CreateSetViewIntent> = Reducer { intent, _ ->
-//        when (intent) {
-//            is CreateSetViewIntent.Start -> fetchSet(intent.heroId, intent.setId)
-//        }
-//    }
-//
-//    private suspend fun fetchSet(heroId: String, setId: String?) {
-//        dataStore.userId.collect { token ->
-//            val user = userRepository.fetchOne(token)
-//            val hero = heroRepository.fetchOne(heroId)
-//            val set = when (setId) {
-//                null -> UserSet("", token, emptyMap(), heroId, emptyList())
-//                else -> setRepository.fetchOne(setId)
-//            }
-//            success(UserSetUseCase(set, user, hero))
-//        }
-//    }
+    private val _useCase = MutableStateFlow<UserSetUseCase?>(null)
+    val useCase = _useCase.asStateFlow()
+
+    fun init(heroId: String, setId: String?) {
+        coroutine {
+            dataStore.userId.collect { token ->
+                _useCase.emit(UserSetUseCase(
+                    user = userRepository.fetchOne(token),
+                    hero = heroRepository.fetchOne(heroId),
+                    set = when (setId) {
+                        null -> UserSet("", token, emptyMap(), heroId, emptyList())
+                        else -> setRepository.fetchOne(setId)
+                    }
+                ))
+            }
+        }
+    }
+
+    fun saveSet(userSet: UserSet) {
+        coroutine { setRepository.update(userSet) }
+    }
 
 }
